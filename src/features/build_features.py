7144150cd927eb3e0a8f9d1e1a0b5bd715a3f745
Georges Alkhouri <alkhouri@informatik.uni-leipzig.de>
2020-01-08 12:40:53 +0100

    [CLEANUP]

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,10 +1,3 @@
-from sklearn.pipeline import make_pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.preprocessing import FunctionTransformer
-
-
 def cast_column_type(df):
     category_features = ['season', 'holiday', 'mnth', 'hr',
                          'weekday', 'workingday', 'weathersit']
@@ -18,20 +11,8 @@ def drop_columns(df):
 
 
 def build_spark_pipeline(df, **kwargs):
-    """Short summary.
-
-    Parameters
-    ----------
-    df : type
-        Description of parameter `df`.
-    **kwargs : type
-        Description of parameter `**kwargs`.
-
-    Returns
-    -------
-    type
-        Description of returned object.
-
+    """Build a pipeline that is executable on a spark cluster. This is just
+    a showcase implementation.
     """
     from pyspark.ml import Pipeline
     from pyspark.ml.feature import OneHotEncoderEstimator
@@ -39,7 +20,6 @@ def build_spark_pipeline(df, **kwargs):
     from pyspark.ml.feature import VectorAssembler
     from pyspark.ml.regression import GBTRegressor
 
-    # ...
     one_hot_encoder = OneHotEncoderEstimator(inputCols=['season', 'yr', 'mnth'],
                                              outputCols=['season_final', 'yr_final', 'mnth_final'])
 
@@ -59,21 +39,16 @@ def build_spark_pipeline(df, **kwargs):
 
 
 def build_sklearn_pipline(df, **kwargs):
-    """Short summary.
-
-    Parameters
-    ----------
-    df : type
-        Description of parameter `df`.
-    **kwargs : type
-        Description of parameter `**kwargs`.
-
-    Returns
-    -------
-    type
-        Description of returned object.
-
+    """Build the sklearn prototype pipeline that is also used in the
+    Jupyter Notebook to generate the stated results in the report.
+    Note: Data preprocessing is done in the Jupyter Notebook and
+    was not implemented back in the project code because of time purposes.
     """
+    from sklearn.pipeline import make_pipeline
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.ensemble import GradientBoostingRegressor
+
     pre_transformer = ColumnTransformer(
         transformers=[('min_max_features', MinMaxScaler(), ['temp', 'hum', 'windspeed',
                                                             'season', 'yr', 'mnth', 'hr',
